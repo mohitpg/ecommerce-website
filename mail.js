be7e54c92ed14357e27f0a81bd865a7db95791e0
Mohitpg <mohitp.gupta1102@gmail.com>
2023-04-20 18:04:08 +0530

    Added track page and added product codes

--- mail.js
+++ mail.js
@@ -3,41 +3,29 @@ const bodyParser= require("body-parser");
 const https = require('https');
 const { stringify } = require("querystring");
 const app=express();
-//app.use(express.static(__dirname));e
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
 })
+var loginstat=false;
 app.get("/login",function(req,res){
     res.sendFile(__dirname+"/login/login.html")
 })
 app.post("/login",function(req,res){
-    let email=req.body.email;
+    let femail=req.body.email;
     let pwd=req.body.pwd;
-    let data={
-        members:[
-            {
-                email_address:email
-            }
-        ]
-    }
-    var jsondata=stringify(data);
-    const url='https://us17.api.mailchimp.com/3.0/lists/2e5bdb1c8c';
-    const options={
-        method:"POST",
-        auth:"mohitpg:736b677ceeca34c64969e4d233c135c3-us17"
-    }
-    const reques= https.request(url,options,function(response){
-        response.on("data",function(data){
-            console.log("yes")
-        })
-    })
-    reques.write(jsondata);
-    reques.end();
+    console.log(femail)
+    loginstat=true;
+    res.redirect("/");
+    
 })
-app.get("/views",function(req,res){
-    res.render("index");
+app.get("/views/trackOrder",function(req,res){
+    if(loginstat==false){
+            res.send("<h1>Sorry! You are not logged in</h1>");
+    }else{
+    res.render("trackOrder/track");}
 })
 
 app.listen(3000,()=>{
